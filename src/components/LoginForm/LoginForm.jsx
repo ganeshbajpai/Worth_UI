ce87d45d4c44d7c988db2e0d53387495d911ca1e
ganesh <ganesh.bajpai@gmail.com>
2024-07-27 12:46:49 +0530

    cred

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -23,7 +23,7 @@ const LoginForm = () => {
         headers: {
           'Content-Type': 'application/json',
         },
-        
+        credentials: 'include',
         body: JSON.stringify({
           email,
           password,
