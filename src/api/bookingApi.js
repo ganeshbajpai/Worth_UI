ea33b5ba48b445c549e10c0db8d058b91a19f4cd
ganesh <ganesh.bajpai@gmail.com>
2024-08-09 15:48:57 +0530

    api

--- src/api/bookingApi.js
+++ src/api/bookingApi.js
@@ -1,5 +1,5 @@
-const booking_url="http://localhost:8086";
-
+// const booking_url="http://localhost:8086";
+const booking_url="https://thesisguru.co.in";
 
 export default booking_url ;
 
--- src/api/customerapi.js
+++ src/api/customerapi.js
@@ -1,3 +1,4 @@
-const customer_url="http://localhost:8085";
+// const customer_url="http://localhost:8085";
+const customer_url="https://thesisguru.co.in";
 export default customer_url;
 
